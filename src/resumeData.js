ebb19f9b08b698e2191c09f34bc87ba2ae9d0ab5
Anastasia Sweet-Saint Jean <asweet@bgsu.edu>
2020-09-10 00:58:18 -0400

    completed updated portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+    "name": "Anastasia Saint Jean",
+    "role": "Digital Marketing Strategist",
+    "linkedinId":"asweet9212@gmail.com",
+    "roleDescription": "I like to plan and execute effective marketing plans to keep brands and companies thriving in their perspective industry.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anastasia-sweet-bb871011a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/asweet27",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am professional Digital Marketing strategist. I have earned my bachelors in Psychology, along with my masters in Digital Media and Communications. Additionally, I have a certificate of completion from the University of Miami in Full Stack Web Development.I am confident that my educational background and professional experience has afforded me the skills necessary to understand a consumers point of view and create content based on their needs. Please enjoy exploring my various accomplishments as it pertains to web development, and feel free to contact me with any additional questions. ",
+    "address":"North Carolina",
+    "website":"github.com/asweet27",
+    "education":[
+      {
+        "UniversityName":"The Florida State University",
+        "specialization":"Digital Media and Communication Studies/Integrated Marketing Communications",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2015",
+     
+      },
+      {
+        "UniversityName":"Bowling Green State University ",
+        "specialization":"Psychology",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2014",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Trilogy Education Services",
+        "specialization":"Admissions Advisor",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        
+      },
+      {
+        "CompanyName":"Health Career Institute",
+        "specialization":"Student Services Coordinator",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2018",
+        
+      },
+      {
+        "CompanyName":"Keiser University",
+        "specialization":"Community Relations Coordinator",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2017",
+        
+      }
+    ],
+    "skillsDescription":"I am well-versed in public relations and building partnerships with key stakeholders",
+    "skills":[
+      {
+        "skillname":"Public Relations"
+      },
+      {
+        "skillname":"Consumer Engagement"
+      },
+      {
+        "skillname":"Consumer Retention"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Weather Dashboard",
+        "description":"App where users can check current weather and get 6-day prediction in any given city",
+        "imgurl":"images/portfolio/weather.png"
+      },
+      {
+        "name":"Password Generator",
+        "description":"App where users can input parameters to generate a random password",
+        "imgurl":"images/portfolio/pwGenerator.png"
+      },
+      {
+        "name":"Daily Planner",
+        "description":"App where users can input tasks to be completed by the hour on a given day",  
+        "imgurl":"images/portfolio/dailyPlanner.png"
+      },
+      {
+        "name":"Code Quiz",
+        "description":"App where users can test their knowledge on the basics of web development",
+        "imgurl":"images/portfolio/codeQ.png"
+      }
+    ]
+  }
+  
+  export default resumeData
